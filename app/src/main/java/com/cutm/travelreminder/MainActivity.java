ae5302b8c39279ee0222201834d7795fc0cc7517
star <rohan.das1203@gmail.com>
2020-11-11 20:18:48 +0530

    Map Updated

--- app/src/main/java/com/cutm/travelreminder/MainActivity.java
+++ app/src/main/java/com/cutm/travelreminder/MainActivity.java
@@ -1,14 +1,200 @@
 package com.cutm.travelreminder;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback
+//        LocationListener,GoogleApiClient.ConnectionCallbacks,
+//        GoogleApiClient.OnConnectionFailedListener
+       {
+           Location currentLocation;
+           FusedLocationProviderClient fusedLocationProviderClient;
+           private static final int REQUEST_CODE = 101;
+           @Override
+           protected void onCreate(Bundle savedInstanceState) {
+               super.onCreate(savedInstanceState);
+               setContentView(R.layout.activity_main);
+               fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+               fetchLocation();
+
+               ProgressBar progressBar1 = findViewById(R.id.mapProgressBar);
+               progressBar1.setProgress(100);
+               progressBar1.setMax(100);
+
+           }
+           private void fetchLocation() {
+               if (ActivityCompat.checkSelfPermission(
+                       this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                       this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                   ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+                   return;
+               }
+               Task<Location> task = fusedLocationProviderClient.getLastLocation();
+               task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                   @Override
+                   public void onSuccess(Location location) {
+                       if (location != null) {
+                           currentLocation = location;
+                           Toast.makeText(getApplicationContext(), currentLocation.getLatitude() + "" + currentLocation.getLongitude(), Toast.LENGTH_SHORT).show();
+                           SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                           assert supportMapFragment != null;
+                           supportMapFragment.getMapAsync(MainActivity.this);
+                       }
+                   }
+               });
+           }
+           @Override
+           public void onMapReady(GoogleMap googleMap) {
+               LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+               MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("I am here!");
+               googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+               googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+               googleMap.addMarker(markerOptions);
+           }
+           @Override
+           public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+               switch (requestCode) {
+                   case REQUEST_CODE:
+                       if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                           fetchLocation();
+                       }
+                       break;
+               }
+           }
+       }
+
+   // GoogleMap map;
+//   private GoogleMap mMap;
+//    Location mLastLocation;
+//    Marker mCurrLocationMarker;
+//    GoogleApiClient mGoogleApiClient;
+//    LocationRequest mLocationRequest;
+//
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//
+//        mapFragment.getMapAsync(this);
+//
+//    }
+//
+//    @Override
+//    public void onMapReady(GoogleMap googleMap) {
+////        map = googleMap;
+////        LatLng odisha = new LatLng(20.560824, 86.396657);
+////        map.addMarker(new MarkerOptions().position(odisha).title("Odisha"));
+////        map.moveCamera(CameraUpdateFactory.newLatLng(odisha));
+//
+//        mMap = googleMap;
+//
+//        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            if (ContextCompat.checkSelfPermission(this,
+//                    Manifest.permission.ACCESS_FINE_LOCATION)
+//                    == PackageManager.PERMISSION_GRANTED) {
+//                buildGoogleApiClient();
+//                mMap.setMyLocationEnabled(true);
+//            }
+//        }
+//        else {
+//            buildGoogleApiClient();
+//            mMap.setMyLocationEnabled(true);
+//        }
+//
+//    }
+//    protected synchronized void buildGoogleApiClient() {
+//        mGoogleApiClient = new GoogleApiClient.Builder(this)
+//                .addConnectionCallbacks(this)
+//                .addOnConnectionFailedListener(this)
+//                .addApi(LocationServices.API).build();
+//        mGoogleApiClient.connect();
+//    }
+//
+//    @Override
+//    public void onConnected(Bundle bundle) {
+//
+//        mLocationRequest = new LocationRequest();
+//        mLocationRequest.setInterval(1000);
+//        mLocationRequest.setFastestInterval(1000);
+//        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+//        if (ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.ACCESS_FINE_LOCATION)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+//        }
+//
+//    }
+//
+//    @Override
+//    public void onConnectionSuspended(int i) {
+//
+//    }
+//
+//    @Override
+//    public void onLocationChanged(Location location) {
+//
+//        mLastLocation = location;
+//        if (mCurrLocationMarker != null) {
+//            mCurrLocationMarker.remove();
+//        }
+//        //Place current location marker
+//        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(latLng);
+//        markerOptions.title("Current Position");
+//        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+//        mCurrLocationMarker = mMap.addMarker(markerOptions);
+//
+//        //move map camera
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+//
+//        //stop location updates
+//        if (mGoogleApiClient != null) {
+//            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+//        }
+//
+//    }
+//
+//    @Override
+//    public void onConnectionFailed(ConnectionResult connectionResult) {
+//
+//    }
+//
+//}
